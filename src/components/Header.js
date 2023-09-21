808b4693f0c4dac9db39aa837f974cc1d206c0ca
raihanbadruz <raihanzaman79@gmail.com>
2023-09-22 02:01:08 +0700

    Kurang Styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,16 @@
-import { useState } from "react";
+import { Link } from "react-router-dom";
 
-const Header = ({ setRefresh }) => {
-  const [task, setTask] = useState("");
-
-  // fungsi untuk menambah data todo melalui API ketika tombol "Add" di klik
-  const addTodo = () => {
-    const newTodo = { task, complete: false };
-
-    fetch("http://localhost:8000/todos", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newTodo),
-    }).then(() => {
-      // ketika sukses menambah data, reset form dengan mengeset state task menjadi empty string
-      setTask("");
-      setRefresh(true);
-
-      setTimeout(() => {
-        alert("new todo added.");
-      }, 500);
-    });
-  };
-
+function Header() {
   return (
-    <div id="todo-header" className="header">
-      <h2>Simple Todo App</h2>
-      <input
-        type="text"
-        value={task}
-        onChange={(e) => setTask(e.target.value)}
-      />
-      <span className="add-button" onClick={addTodo}>
-        Add
-      </span>
-    </div>
+    <>
+      <div className="header mt-2">
+        <h2>TodoList Website</h2>
+        <Link to="/new">
+          <span className="add-new">Add Task</span>
+        </Link>
+      </div>
+    </>
   );
-};
+}
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Routing from "./router/Routing";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "new",
+    element: <Routing />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
